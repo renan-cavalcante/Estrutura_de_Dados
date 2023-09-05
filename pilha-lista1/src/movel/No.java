22d07c096a6c5d7dc06f168385858b5d688d8f10
renan-cavalcante <renancggama@gmail.com>
2023-09-04 21:22:19 -0300

    pilhas int e string

---
+++ pilha-lista1/src/movel/No.java
@@ -0,0 +1,11 @@
+package movel;
+
+public class No {
+	int dado;
+	No no;
+	
+	public No(int valor, No no) {
+		this.no = no;
+		dado = valor;
+	}
+}
---
+++ pilha-lista2/pilhaString/src/movel/No.java
@@ -0,0 +1,11 @@
+package movel;
+
+public class No {
+	String dado;
+	No no;
+	
+	public No(String valor, No no) {
+		this.no = no;
+		dado = valor;
+	}
+}
