aa9b4748c5207f6ff3967ecea4010f60ef176e45
renan-cavalcante <renancggama@gmail.com>
2023-09-04 21:41:26 -0300

    exercicio4

---
+++ pilha-lista2/exercicio4/src/exercicio4/view/Program.java
@@ -0,0 +1,15 @@
+package exercicio4.view;
+
+import exercicio4.controller.ConverterController;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		ConverterController converterController = new ConverterController();
+
+		System.out.println(converterController.decToBin(19));
+
+	}
+
+}
